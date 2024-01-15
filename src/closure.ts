d55c991b4f63891c984f32d1b867a45ba0e2cab3
spetushkov <sergey.petushkov@gmail.com>
2024-01-15 14:53:12 +0100

    feat: :rocket: #NOREF-0

--- src/closure.ts
+++ src/closure.ts
@@ -1,12 +1,13 @@
 // npx ts-node src/closure.ts
 
+// self-invoking function, it runs only once, it sets the counter to zero (0), and returns a function expression
 const add = (function () {
   let counter = 0;
   return function () {
     counter += 1;
     return counter;
   };
-})(); // self-invoking function, it runs only once, it sets the counter to zero (0), and returns a function expression
+})();
 
 let result = add();
 result = add();
---
+++ src/temp.ts
@@ -0,0 +1,15 @@
+// npx ts-node src/temp.ts
+
+const obj1 = {
+  type: 'type1',
+  value: 'value1',
+};
+
+const obj2 = {
+  type: 'type2',
+  value: 'value2',
+};
+
+type ObjType = 
+
+export {};
